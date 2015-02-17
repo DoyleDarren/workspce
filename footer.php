d37878554d893123299eabc86933632567669850
denzildoyle <yell@denzildoyle.me>
2015-02-17 10:22:15 -0400

    Add col and row to footer to maintain page alignment

--- footer.php
+++ footer.php
@@ -1,8 +1,11 @@
-			<div class="wrap">
-	            <footer id="bottom">
-	                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
-	            </footer>
-		    </div>
-        </div>
+		<div class="row">
+		  	<div class="col-md-6 col-md-offset-3">
+				<div class="wrap">
+		            <footer id="bottom">
+		                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
+		            </footer>
+			    </div>
+	        </div>
+	    </div>
     </body>
 </html>
