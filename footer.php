7a5f6d9da27b9319c9f77caf8d7e6e5dcd154614
denzildoyle <yell@denzildoyle.me>
2015-03-08 10:01:43 -0400

    Center text in page footer

--- footer.php
+++ footer.php
@@ -1,11 +1,7 @@
 		<div class="container">
-		  	<div class="col-md-8 col-md-offset-2">
-				<div class="wrap">
-		            <footer id="bottom">
-		                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
-		            </footer>
-			    </div>
-	        </div>
+            <footer id="bottom">
+                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
+            </footer>
 	    </div>
     </body>
 </html>
